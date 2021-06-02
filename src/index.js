7b39fa8a7a928c4217f8de082b74421931146741
Samuel De La Hoz <samueldlh.dev@gmail.com>
2021-06-01 21:01:23 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const { PORT } = require('./config')
+
+const app = express();
+
+const router = require('./routes');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(router);
+
+app.listen(PORT, () => console.log(`server on port: ${PORT}`))
