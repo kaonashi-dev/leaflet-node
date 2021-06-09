f85baaa79bcb90c15ed0772e850e7c6a7f665c33
samueldlh <samueldlh.dev@gmail.com>
2021-06-08 22:39:59 -0500

    locationfound, connections

--- src/public/main.js
+++ src/public/main.js
@@ -1,4 +1,6 @@
 var user = prompt("Escribe tu nombre");
+var userLocation = null;
+var userMarker = null;
 
 const socket = io.connect();
 
@@ -13,19 +15,30 @@ L.marker([10.996730821187757, -74.79675837301376]).addTo(map)
    .bindPopup('TAE')
    .openPopup();
 
-// setTimeout(() => {
-map.locate({ enableHighAccuracy: true });
-map.on('locationfound', (e) => {
-   const radius = e.accuracy;
-   const lat = e.latlng.lat;
-   const lng = e.latlng.lng;
-   L.circle(e.latlng, radius).addTo(map);
-   L.marker([lat, lng]).addTo(map)
-      .bindPopup(user)
-      .openPopup();
-   socket.emit('userLocation', {location: e.latlng, name: user});
-});
-// }, 3000)
+function getLocation() {
+   map.locate({ enableHighAccuracy: true });
+   map.on('locationfound', (e) => {
+      const lat = e.latlng.lat;
+      const lng = e.latlng.lng;
+      console.log(userLocation);
+      if (userMarker) {
+         map.removeLayer(userMarker);
+         userMarker = L.marker([lat, lng]);
+         userMarker.bindPopup(user);
+         map.addLayer(userMarker);
+         socket.emit('userLocation', { location: e.latlng, name: user });
+      } else {
+         userMarker = L.marker([lat, lng]);
+         userMarker.bindPopup(user);
+         map.addLayer(userMarker);
+      }
+   });
+}
+getLocation();
+setInterval(() => {
+   console.log('cambio');
+   getLocation();
+}, 9000);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
    maxZoom: 25,
@@ -33,7 +46,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(map);
 
 socket.on('newUserLocation', (data) => {
-   console.log(123123132);
    const newUserMarker = L.marker([data.location.lat, data.location.lng]);
    newUserMarker.bindPopup(data.name);
    map.addLayer(newUserMarker);
--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,17 @@
 module.exports = (io) => {
    io.on('connection', (socket) => {
       console.log('nuevo usuario conectado');
-      
+
       socket.on('userLocation', (data) => {
          console.log('usuario', data);
          socket.broadcast.emit('newUserLocation', data)
+      });
+
+      socket.on('disconnect', function () {
+
+         console.log('desconectado');
+         socket.emit('disconnected');
+
       });
    });
 }
