751b538baf158ff88081313408d5dd49c83a1afd
Samuel De La Hoz <samueldlh.dev@gmail.com>
2021-06-09 16:27:27 -0500

    interval request

--- src/public/map.js
+++ src/public/map.js
@@ -39,7 +39,7 @@ getLocation();
 setInterval(() => {
    console.log('cambio');
    getLocation();
-}, 9000);
+}, 3000);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
    maxZoom: 25,
